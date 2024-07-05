e8092b2c35e0f4ac65b60893e9cc3abf8ddc77ce
SebasCalderon27 <168150875+SebasCalderon27@users.noreply.github.com>
2024-07-05 15:34:55 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,9 +1,23 @@
+#ifndef FUNCIONES_H
 #define FUNCIONES_H
 
 #include <stdio.h>
 
-void listarProductos(FILE *archivo, char productos[][3][40], double precio[], int indice[]);
-void buscarXMarca(FILE *archivo, char productos[][3][40], double precio[]);
-void buscarXCategoria(FILE *archivo, char productos[][3][40], double precio[]);
-void buscarXPrecioMenor(FILE *archivo, char productos[][3][40], double precio[]);
-void editarProducto(FILE *archivo, char productos[][3][40], double precio[], int indice[]);
+#define MAX_PRODUCTOS 100  // Límite máximo de productos
+
+typedef struct {
+    char nombre[40];
+    char categoria[40];
+    char marca[40];
+    double precio;
+} Producto;
+
+void listarProductos(FILE *archivo, Producto *productos, int numProductos);
+void buscarXMarca(FILE *archivo, Producto *productos, int numProductos);
+void buscarXCategoria(FILE *archivo, Producto *productos, int numProductos);
+void buscarXPrecioMenor(FILE *archivo, Producto *productos, int numProductos);
+void editarProducto(FILE *archivo, Producto *productos, int numProductos);
+void agregarProducto(FILE *archivo, Producto *productos, int *numProductos);
+void eliminarProducto(FILE *archivo, Producto *productos, int *numProductos);
+
+#endif /* FUNCIONES_H */
